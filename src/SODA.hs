f762de6d795ef0eb71e3e27e5bdbcd8ad758b37b
adamConnerSax <adam_conner_sax@yahoo.com>
2016-12-21 16:05:06 -0500

    Switched QueryParams to use map and created beginning of SQL query.

--- src/SODA.hs
+++ src/SODA.hs
@@ -19,7 +19,10 @@ import qualified Data.Text as T
 import Data.Monoid ((<>))
 import Data.Char (isLower,isDigit)
 import Data.List (foldl')
+import qualified Data.Map as M 
 import Control.Monad.Except (runExceptT)
+
+
 
 import           Data.Typeable (Typeable)
 import           GHC.TypeLits (Symbol,KnownSymbol)
@@ -34,59 +37,67 @@ validateIdentifier (DatasetIdentifier di) = if (rightLength di && rightChars di)
   rightChar c = isLower c || isDigit c
   rightChars t = T.foldl (\b c-> b && (rightChar c)) True t
 
+type QueryMap a = M.Map T.Text a
+
 type SocrataColumnName = T.Text
 type SocrataValue = T.Text -- how do we type this
 
 data SocrataFilter  = SocrataFilter { column::SocrataColumnName, value::SocrataValue }
 
-newtype SocrataFilterSet = SocrataFilterSet [SocrataFilter]
---instance ToHttpApiData SocrataFilterSet where
---  toUrlPiece (SocrataFilterSet fs) = (T.intercalate "&" $ ((\(SocrataFilter c v)->c <> "=" <> v) <$> fs))
+type SocrataFilters = QueryMap SocrataValue
 
-toMultiQuery::SocrataFilterSet -> [(T.Text,SocrataValue)]
-toMultiQuery (SocrataFilterSet fs) = map (\(SocrataFilter c v) -> (c,v)) fs
+addFilter::SocrataFilter->SocrataFilters->SocrataFilters
+addFilter (SocrataFilter c v) qm = M.insert c v qm
 
 newtype SocrataResult = SocrataResult T.Text deriving (Show,Generic)
 instance FromJSON SocrataResult
 
+type SoQL = T.Text
+
 data MultiQueryParams (sym :: Symbol) a
     deriving Typeable
-
 
 instance (KnownSymbol sym, ToHttpApiData a, HasClient api)
       => HasClient (MultiQueryParams sym a :> api) where
 
   type Client (MultiQueryParams sym a :> api) =
-    [(T.Text,a)] -> Client api
+    QueryMap a -> Client api
 
-  clientWithRoute Proxy req queryList = -- queryList is [(T.Text,a)]
+  clientWithRoute Proxy req queryMap = 
     clientWithRoute (Proxy :: Proxy api)
                     (foldl' (\ req' (qn,mqv) -> appendToQueryString qn mqv req')
                             req
-                            queryList'
+                            queryList
                     )
 
-    where queryList' = map (\(x,y)->(x,Just $ toQueryParam y)) queryList
+    where queryList = M.toList ((Just . toQueryParam) <$> queryMap) 
 
 
-type SodaAPI = "resource" :> Capture "datasetID" DatasetIdentifier :> MultiQueryParams "filter" SocrataValue :> Get '[JSON] SocrataResult
+type SodaAPI = "resource" :> Capture "datasetID" DatasetIdentifier :> MultiQueryParams "filter" SocrataValue :> Get '[JSON] SocrataResult :<|>
+               "resource" :> Capture "datasetID" DatasetIdentifier :> QueryParam "$query" SoQL :> Get '[JSON] SocrataResult 
 sodaApi::Proxy SodaAPI
 sodaApi = Proxy
 
-filterReq = client sodaApi
+filterReq :<|> soqlReq= client sodaApi
 
 testQuery :: Manager->ClientM SocrataResult
 testQuery mgr = do
   let filterZip = SocrataFilter "zip_code" "11215"
       filterStreet = SocrataFilter "on_street_name" "PRESIDENT STREET"
-      filters = SocrataFilterSet [filterZip,filterStreet]
+      filters = addFilter filterZip . addFilter filterStreet $ M.empty 
       baseUrl = BaseUrl Http "data.cityofnewyork.us" 80 ""
-  filterReq (DatasetIdentifier "qiz3axqb") (toMultiQuery filters) mgr baseUrl
+  filterReq (DatasetIdentifier "qiz3axqb") filters mgr baseUrl
+
+testSoQL :: Manager->ClientM SocrataResult
+testSoQL mgr = do
+  let sql = "SELECT date,number_of_pedestrians_killed WHERE number_of_pedestrians_killed > 1 "
+      baseUrl = BaseUrl Http "data.cityofnewyork.us" 80 ""
+  soqlReq (DatasetIdentifier "qiz3axqb") (Just sql) mgr baseUrl 
 
 testRun:: IO ()
 testRun = do
   mgr <- newManager defaultManagerSettings
-  result <- runExceptT $ testQuery mgr
+  result <- runExceptT $ testSoQL mgr
   case result of
     Left err -> putStrLn $ "Error: " ++ (show err)
     Right (SocrataResult t) -> putStrLn . T.unpack $ "Result: \n" <> t
